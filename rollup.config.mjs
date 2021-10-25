af6730407d2e3812357c7436975a68bd23f160c6
Kornél Horváth <kornel.horvath@me.com>
2021-10-25 20:36:07 +0200

    Updated project files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,11 @@
 import { readFileSync } from 'fs';
+import { defineConfig } from 'rollup';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import esbuild from 'rollup-plugin-esbuild';
 
 const pkg = JSON.parse(readFileSync(new URL('./package.json', import.meta.url)));
 
-export default {
+export default defineConfig({
 	external: [ /node_modules/u ],
 	input: 'src/index.js',
 	plugins: [
@@ -12,14 +13,13 @@ export default {
 		esbuild({
 			exclude: '**/*',
 			minify: true,
-			sourcemap: false
+			sourceMap: false
 		})
 	],
 	output: [
 		{
 			generatedCode: 'es2015',
-			format: 'es',
 			file: pkg.exports
 		}
 	]
-};
+});
