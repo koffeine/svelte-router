c17fe285b5b6d398036bf06d38a2f8c031e959da
Kornél Horváth <kornel.horvath@me.com>
2025-05-09 08:18:02 +0200

    Replaced rollup with vite

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+/** @type {import('vite').UserConfigExport} */
+export default {
+	build: {
+		target: 'esnext',
+		rollupOptions: {
+			external: [
+				'regexparam',
+				'svelte/store'
+			]
+		},
+		lib: {
+			entry: 'src/index.js',
+			formats: [ 'es' ]
+		},
+		reportCompressedSize: false
+	}
+};
