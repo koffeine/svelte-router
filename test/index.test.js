c2aaafdfdac44a136d11928cf7390d1585c085d5
Kornél Horváth <kornel.horvath@me.com>
2025-08-30 19:28:29 +0200

    Replaced mocha with vitest

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
 import jsdomGlobal from 'jsdom-global';
-import { it } from 'mocha';
+import { test } from 'vitest';
 import assert from 'assert/strict';
 import { get } from 'svelte/store';
 import * as Router from '../src/index.js';
@@ -29,58 +29,58 @@ await Router.init([
 ]);
 
 
-it('should handle initial navigation', () => {
+test('should handle initial navigation', () => {
 	check({ path: '/', component: 'Index' });
 });
 
-it('should handle navigation with no params', async () => {
+test('should handle navigation with no params', async () => {
 	await Router.navigate('/no-params');
 
 	check({ path: '/no-params', component: 'NoParams' });
 });
 
-it('should handle navigation with params #1', async () => {
+test('should handle navigation with params #1', async () => {
 	await Router.navigate('/params/:param', { params: { param: 'value' } });
 
 	check({ path: '/params/value', component: 'Params', params: { param: 'value' } });
 });
 
-it('should handle navigation with params #2', async () => {
+test('should handle navigation with params #2', async () => {
 	await Router.navigate('/params/value');
 
 	check({ path: '/params/value', component: 'Params', params: { param: 'value' } });
 });
 
-it('should handle navigation with optional params', async () => {
+test('should handle navigation with optional params', async () => {
 	await Router.navigate('/optional-params');
 
 	check({ path: '/optional-params', component: 'OptionalParams', params: { param: null } });
 });
 
-it('should handle navigation with query params #1', async () => {
+test('should handle navigation with query params #1', async () => {
 	await Router.navigate('/query-params', { query: { param: 'value' } });
 
 	check({ path: '/query-params', component: 'QueryParams', query: { param: 'value' } });
 });
 
-it('should handle navigation with query params #2', async () => {
+test('should handle navigation with query params #2', async () => {
 	await Router.navigate('/query-params?param=value');
 
 	check({ path: '/query-params', component: 'QueryParams', query: { param: 'value' } });
 });
 
-it('should handle navigation with redirect', async () => {
+test('should handle navigation with redirect', async () => {
 	await Router.navigate('/redirect');
 
 	check({ path: '/redirected', component: 'Redirected' });
 });
 
-it('should handle wildcard navigation', async () => {
+test('should handle wildcard navigation', async () => {
 	await Router.navigate('/wildcard/any/thing');
 
 	check({ path: '/wildcard/any/thing', component: 'Wildcard', params: { '*': 'any/thing' } });
 });
 
-it('should throw error on unknown path', async () => {
+test('should throw error on unknown path', async () => {
 	await assert.rejects(Router.navigate('/not-found'));
 });
