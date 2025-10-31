8b36a38c3cec9208be286f0f3bf28c6418fc80bb
Kornél Horváth <kornel.horvath@me.com>
2025-10-31 16:08:19 +0100

    Updated dependencies

--- vitest.config.js
+++ vitest.config.js
@@ -2,11 +2,12 @@ import { mergeConfig } from 'vite';
 import { svelteTesting } from '@testing-library/svelte/vite';
 import viteConfig from './vite.config.js';
 
-/** @type {import('vitest/config').UserConfigExport} */
+/** @type {import('vitest/config').ViteUserConfigExport} */
 const vitestConfig = {
 	plugins: [ svelteTesting() ],
 	test: {
 		environment: 'happy-dom',
+		reporters: 'tree',
 		coverage: {
 			enabled: true,
 			include: 'src/**/*.js',
